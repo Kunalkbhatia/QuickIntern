8704bbca27109a790a8f59393fa6aacb3e563332
Kunal Bhatia <bhatiakbkb@gmail.com>
2024-06-14 12:20:33 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      "College": "Chandigarh University",
+      "Degree" : "Bachelor of Engineerin (B.E)",
+      "Stream" : "Computer Science Engineering",
+      "Percentage" : "80",
+      "Training" : "Data Structures and Algorithms, Web Development",
+      "Organization" : "XpressBites",
+      "description" : "During my 5-month internship at Xpressbites Technologies, I played a key role in developing a custom API for the merchant analysis dashboard, enhancing decision-making and onboarding processes. I also contributed to frontend development, using Chart.js for intuitive data visualization. I proactively resolve critical bugs, reducing platform downtime and ensuring a seamless user experience.Additionally, I utilized ReactJS and React Styled Components to design and create new components for enhanced user experiences.",
+      "link" : "https://github.com/Kunalkbhatia",
+      "hiringReason" : "Over the past few years, I have acquired relevant skills and experience, which I shall bring to your organization. I have also worked tirelessly on my communication abilities and teamwork skills, which I will put to use in my future career, which would be in your organization if I am selected for the position.",
+      "availability" : "Yes, I will be available",
+      "rating" : "4",
+    }
+]
---
+++ secret.js
@@ -0,0 +1,3 @@
+let id = "bhatiakbkb@gmail.com";
+let pass = "Kunal2301@";
+module.exports = {id, pass};
